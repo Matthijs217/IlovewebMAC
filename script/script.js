a659f3831d6c08166eea6a18e40180bfc416dc76
Matthijs217 <matthijstenbrink@gmail.com>
2025-01-07 09:41:10 +0100

    **

--- script/script.js
+++ script/script.js
@@ -4,6 +4,11 @@ inputField.addEventListener("keydown", showPassword);
 
 function showPassword(event) { // Event doorgeven als parameter aan de functie
     if (event.key === "Enter") {
-        window.location.href = "mainpage.html";
+        const inputValue = inputField.value; // Haal de waarde van het invoerveld op
+        if (inputValue === "Matthijs") { // Controleer of de invoer "Matthijs" is
+            window.location.href = "mainpage.html";
+        } else {
+            alert("Onjuiste invoer, probeer opnieuw."); // Geef een foutmelding
+        }
     }
 }
