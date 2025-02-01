2b6436a54f94ba9a104bde09f566d9bc8923daa0
Matthijs217 <matthijstenbrink@gmail.com>
2025-02-01 12:25:01 +0100

    **

--- script/mainpage.js
+++ script/mainpage.js
@@ -1,10 +1,17 @@
     // Selecteer de folder1 icoon en app1 sectie
+    let body = document.querySelector('body');
     let folder1 = document.querySelector('#folder1');
     let app1 = document.querySelector('.app1'); // Selecteer de app1 sectie
 
-    // Voeg een click event listener toe aan folder1
-    folder1.addEventListener('click', () => {
-        // Toggle de 'visible' class op de app1 sectie
-        app1.classList.add('visible');
+    folder1.addEventListener('click', (event) => {
+        // Stop de click event van het doorgeven aan de body
+        event.stopPropagation();
+        app1.classList.toggle('visible');
     });
 
+    body.addEventListener('click', () => {
+        // Verwijder de 'visible' class van de app1 sectie als deze aanwezig is
+        if (app1.classList.contains('visible')) {
+            app1.classList.remove('visible');
+        }
+    });
